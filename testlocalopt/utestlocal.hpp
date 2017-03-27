65bb0bf1eddc08cbeaa15eb763b4225121ad507f
tenkjm <atour07@yandex.ru>
2017-03-27 16:17:35 +0300

    5 functions on 5 search methods test Need methodics

--- testlocalopt/utestlocal.hpp
+++ testlocalopt/utestlocal.hpp
@@ -53,57 +53,61 @@ const char* JSONPATH;
 
 using namespace snowgoose::expression;
 using namespace OPTITEST;
-  
-  class MyStopper : public LOCSEARCH::QuadLS<double>::Stopper {
-    public:
 
-        bool stopnow(double s, int k, double vo, double vn) {
-            mCnt++;
-            if (s < 1e-3)
-                return true;
-            else if (k > 16)
-                return true;
-            else
-                return false;
-        }
+class MyStopper : public LOCSEARCH::QuadLS<double>::Stopper {
+public:
 
-        int mCnt = 0;
-    };
+    bool stopnow(double s, int k, double vo, double vn) {
+        mCnt++;
+        if (s < 1e-3)
+            return true;
+        else if (k > 16)
+            return true;
+        else
+            return false;
+    }
 
+    int mCnt = 0;
+};
 
 class GlobOptTest : public ::testing::Test {
- protected:
+protected:
 
-  GlobOptTest() : dfr(JSONPATH)
-  {
-  }
-  
-  virtual void SetUp() 
-  {
-		
-  }
-  
-  virtual void TearDown() 
-  {
-      
-  }
+    GlobOptTest() : dfr(JSONPATH) {
+    }
 
-  void testglobopt(const std::string& key,const Expr<double>& expr, const Expr<Interval<double>> & exprInterval, int customDim = 0, double epsilon = EPSILON) 
-  {
+    virtual void SetUp() {
 
-    cout<<"inside\n";
-    auto desc = dfr.getdesr(key);
-    int dim = desc.anyDim ? customDim : desc.dim;
-    Bounds bounds = desc.anyDim ? Bounds(dim, desc.bounds[0]) : desc.bounds;
+    }
 
-    // Setup problem
-    OPTITEST::ExprProblemFactory fact(expr, bounds);
-    COMPI::MPProblem<double> *mpp = fact.getProblem();
-		
-    double expected = desc.globMinY;
-    cout<<"inside2\n";
-     int cnt = 0;
-     int n = mpp->mBox->mDim;
+    virtual void TearDown() {
+
+    }
+
+    void testglobopt(const std::string& key, const Expr<double>& expr, const Expr<Interval<double>> &exprInterval, int customDim = 0, double epsilon = EPSILON) {
+
+        testgloboptCoorHJExplorer(key, expr, exprInterval, customDim);
+        testgloboptTestVarHJ(key, expr, exprInterval, customDim);
+        testgloboptTestHJLinear(key, expr, exprInterval, customDim);
+        testgloboptTestCoorDesk(key, expr, exprInterval, customDim);
+        testgloboptTestVarCoorDesk(key, expr, exprInterval, customDim);
+    }
+    
+    void testgloboptCoorHJExplorer(const std::string& key, const Expr<double>& expr, const Expr<Interval<double>> &exprInterval, int customDim = 0, double epsilon = EPSILON) {
+
+        cout << "HkeJeeves Coord explorer\n";
+        auto desc = dfr.getdesr(key);
+        int dim = desc.anyDim ? customDim : desc.dim;
+        Bounds bounds = desc.anyDim ? Bounds(dim, desc.bounds[0]) : desc.bounds;
+
+        // Setup problem
+        OPTITEST::ExprProblemFactory fact(expr, bounds);
+        COMPI::MPProblem<double> *mpp = fact.getProblem();
+
+        double expected = desc.globMinY;
+        cout << "inside2\n";
+        int cnt = 0;
+        int n = mpp->mBox->mDim;
         double *x = new double[n];
         snowgoose::BoxUtils::getCenter(*(mpp->mBox), x);
         double v;
@@ -113,9 +117,16 @@ class GlobOptTest : public ::testing::Test {
         //explr.getOptions().mResetEveryTime = false;
 
         snowgoose::BoxUtils::getCenter(*(mpp->mBox), x);
-
+        mpp->mObjectives[0] = new COMPI::FuncCnt<double>(*(mpp->mObjectives.at(0)));
         v = mpp->mObjectives[0]->func(x);
-cout<<"inside3\n";
+        cout << "inside3\n";
+        
+        cout << "inside33\n";
+        COMPI::MPProblem<double> pr = *mpp;
+        COMPI::FuncCnt<double> *obj = dynamic_cast<COMPI::FuncCnt<double>*> ((pr).mObjectives[0]);
+        cout << "inside331\n";
+        obj->reset();
+        cout << "inside333\n";
         LOCSEARCH::MHookeJeeves<double> hjdesc(*(mpp), explr);
         hjdesc.getOptions().mLambda = 0;
         hjdesc.getOptions().mInc = 1;
@@ -123,36 +134,277 @@ cout<<"inside3\n";
         hjdesc.setStopper([&](double v, const double* x) {
             return false;
         });
-
+        
         hjdesc.search(x, v);
-        
-        cout<<"inside5\n";
-        
-    ASSERT_NEAR(expected, v, epsilon);
-}
+        std::cout << hjdesc.about() << "\n";
+        std::cout << "In " << obj->mCounters.mFuncCalls << " function calls found v = " << v << "\n";
+        std::cout << " at " << snowgoose::VecUtils::vecPrint(n, x) << "\n";
+        cout << "inside5\n";
 
-DescFuncReader dfr;
+        ASSERT_NEAR(expected, v, epsilon);
+    }
+    
+    
+    
+    
+    
+    void testgloboptTestVarHJ(const std::string& key, const Expr<double>& expr, const Expr<Interval<double>> &exprInterval, int customDim = 0, double epsilon = EPSILON) {
+
+        cout << "HkeJeeves VarCoord explorer\n";
+        auto desc = dfr.getdesr(key);
+        int dim = desc.anyDim ? customDim : desc.dim;
+        Bounds bounds = desc.anyDim ? Bounds(dim, desc.bounds[0]) : desc.bounds;
+
+        // Setup problem
+        OPTITEST::ExprProblemFactory fact(expr, bounds);
+        COMPI::MPProblem<double> *mpp = fact.getProblem();
+
+        double expected = desc.globMinY;
+        cout << "inside2\n";
+        int cnt = 0;
+        int n = mpp->mBox->mDim;
+        double *x = new double[n];
+        snowgoose::BoxUtils::getCenter(*(mpp->mBox), x);
+        double v;
+        LOCSEARCH::VarCoorHJExplorer<double> explr(*(mpp));
+        //explr.getOptions().mHInit = 0.01;
+        //explr.getOptions().mHLB = 1e-6;
+        //explr.getOptions().mResetEveryTime = false;
+
+        snowgoose::BoxUtils::getCenter(*(mpp->mBox), x);
+        mpp->mObjectives[0] = new COMPI::FuncCnt<double>(*(mpp->mObjectives.at(0)));
+        v = mpp->mObjectives[0]->func(x);
+        cout << "inside3\n";
+        
+        cout << "inside33\n";
+        COMPI::MPProblem<double> pr = *mpp;
+        COMPI::FuncCnt<double> *obj = dynamic_cast<COMPI::FuncCnt<double>*> ((pr).mObjectives[0]);
+        cout << "inside331\n";
+        obj->reset();
+        cout << "inside333\n";
+        LOCSEARCH::MHookeJeeves<double> hjdesc(*(mpp), explr);
+        hjdesc.getOptions().mLambda = 0;
+        hjdesc.getOptions().mInc = 1;
+        hjdesc.getOptions().mDec = 1;
+        hjdesc.setStopper([&](double v, const double* x) {
+            return false;
+        });
+        
+        hjdesc.search(x, v);
+        std::cout << hjdesc.about() << "\n";
+        std::cout << "In " << obj->mCounters.mFuncCalls << " function calls found v = " << v << "\n";
+        std::cout << " at " << snowgoose::VecUtils::vecPrint(n, x) << "\n";
+        cout << "inside5\n";
+
+        ASSERT_NEAR(expected, v, epsilon);
+    }
+
+    
+    void testgloboptTestHJLinear(const std::string& key, const Expr<double>& expr, const Expr<Interval<double>> &exprInterval, int customDim = 0, double epsilon = EPSILON) {
+
+        cout << "HkeJeeves Linear explorer\n";
+        auto desc = dfr.getdesr(key);
+        int dim = desc.anyDim ? customDim : desc.dim;
+        Bounds bounds = desc.anyDim ? Bounds(dim, desc.bounds[0]) : desc.bounds;
+
+        // Setup problem
+        OPTITEST::ExprProblemFactory fact(expr, bounds);
+        COMPI::MPProblem<double> *mpp = fact.getProblem();
+
+        double expected = desc.globMinY;
+        cout << "inside2\n";
+        int cnt = 0;
+        int n = mpp->mBox->mDim;
+        double *x = new double[n];
+        snowgoose::BoxUtils::getCenter(*(mpp->mBox), x);
+        double v;
+        LOCSEARCH::CoorHJExplorer<double>  explr(*(mpp));
+        //explr.getOptions().mHInit = 0.01;
+        //explr.getOptions().mHLB = 1e-6;
+        //explr.getOptions().mResetEveryTime = false;
+
+        snowgoose::BoxUtils::getCenter(*(mpp->mBox), x);
+        mpp->mObjectives[0] = new COMPI::FuncCnt<double>(*(mpp->mObjectives.at(0)));
+        v = mpp->mObjectives[0]->func(x);
+        cout << "inside3\n";
+        
+        cout << "inside33\n";
+        COMPI::MPProblem<double> pr = *mpp;
+        COMPI::FuncCnt<double> *obj = dynamic_cast<COMPI::FuncCnt<double>*> ((pr).mObjectives[0]);
+        cout << "inside331\n";
+        obj->reset();
+        cout << "inside333\n";
+        LOCSEARCH::MHookeJeevesLinear<double> hjdesc(*(mpp), explr);
+        hjdesc.getOptions().mLambda = 0;
+        hjdesc.getOptions().mInc = 1;
+        hjdesc.getOptions().mDec = 1;
+        hjdesc.setStopper([&](double v, const double* x) {
+            return false;
+        });
+        
+        hjdesc.search(x, v);
+        std::cout << hjdesc.about() << "\n";
+        std::cout << "In " << obj->mCounters.mFuncCalls << " function calls found v = " << v << "\n";
+        std::cout << " at " << snowgoose::VecUtils::vecPrint(n, x) << "\n";
+        cout << "inside5\n";
+
+        ASSERT_NEAR(expected, v, epsilon);
+    }
+
+
+    void testgloboptTestCoorDesk(const std::string& key, const Expr<double>& expr, const Expr<Interval<double>> &exprInterval, int customDim = 0, double epsilon = EPSILON) {
+
+        
+        
+        
+        int cnt = 0;
+        auto stopper = [&](double xdiff, double fdiff, double gran, double fval, int n) {
+            cnt++;
+            //std::cout << "cnt = " << cnt << ", fval =" << fval << "\n";
+            if (cnt > 7000)
+                return true;
+            else
+                return false;
+        };
+        cout << "TestCoorDesk explorer\n";
+        auto desc = dfr.getdesr(key);
+        int dim = desc.anyDim ? customDim : desc.dim;
+        Bounds bounds = desc.anyDim ? Bounds(dim, desc.bounds[0]) : desc.bounds;
+
+        // Setup problem
+        OPTITEST::ExprProblemFactory fact(expr, bounds);
+        COMPI::MPProblem<double> *mpp = fact.getProblem();
+
+        double expected = desc.globMinY;
+        cout << "inside2\n";
+         
+        int n = mpp->mBox->mDim;
+        double *x = new double[n];
+        snowgoose::BoxUtils::getCenter(*(mpp->mBox), x);
+        double v;
+        LOCSEARCH::CoorDesc<double> desk(*mpp);
+        //explr.getOptions().mHInit = 0.01;
+        //explr.getOptions().mHLB = 1e-6;
+        //explr.getOptions().mResetEveryTime = false;
+
+        snowgoose::BoxUtils::getCenter(*(mpp->mBox), x);
+        mpp->mObjectives[0] = new COMPI::FuncCnt<double>(*(mpp->mObjectives.at(0)));
+        v = mpp->mObjectives[0]->func(x);
+        cout << "inside3\n";
+        
+        cout << "inside33\n";
+        COMPI::MPProblem<double> pr = *mpp;
+        COMPI::FuncCnt<double> *obj = dynamic_cast<COMPI::FuncCnt<double>*> ((pr).mObjectives[0]);
+        cout << "inside331\n";
+        obj->reset();
+        cout << "inside333\n";
+        
+        
+        desk.search(x, v);
+         
+        std::cout << "In " << obj->mCounters.mFuncCalls << " function calls found v = " << v << "\n";
+        std::cout << " at " << snowgoose::VecUtils::vecPrint(n, x) << "\n";
+        cout << "inside5\n";
+
+        ASSERT_NEAR(expected, v, epsilon);
+    }
+    
+     void testgloboptTestVarCoorDesk(const std::string& key, const Expr<double>& expr, const Expr<Interval<double>> &exprInterval, int customDim = 0, double epsilon = EPSILON) {
+
+        
+        
+        
+        int cnt = 0;
+        auto stopper = [&](double xdiff, double fdiff, double gran, double fval, int n) {
+            cnt++;
+            //std::cout << "cnt = " << cnt << ", fval =" << fval << "\n";
+            if (cnt > 7000)
+                return true;
+            else
+                return false;
+        };
+        cout << "TestVarCoorDesk explorer\n";
+        auto desc = dfr.getdesr(key);
+        int dim = desc.anyDim ? customDim : desc.dim;
+        Bounds bounds = desc.anyDim ? Bounds(dim, desc.bounds[0]) : desc.bounds;
+
+        // Setup problem
+        OPTITEST::ExprProblemFactory fact(expr, bounds);
+        COMPI::MPProblem<double> *mpp = fact.getProblem();
+
+        double expected = desc.globMinY;
+        cout << "inside2\n";
+         
+        int n = mpp->mBox->mDim;
+        double *x = new double[n];
+        snowgoose::BoxUtils::getCenter(*(mpp->mBox), x);
+        double v;
+        LOCSEARCH::VarCoorDesc<double> desk(*mpp);
+        //explr.getOptions().mHInit = 0.01;
+        //explr.getOptions().mHLB = 1e-6;
+        //explr.getOptions().mResetEveryTime = false;
+
+        snowgoose::BoxUtils::getCenter(*(mpp->mBox), x);
+        mpp->mObjectives[0] = new COMPI::FuncCnt<double>(*(mpp->mObjectives.at(0)));
+        v = mpp->mObjectives[0]->func(x);
+        cout << "inside3\n";
+        
+        cout << "inside33\n";
+        COMPI::MPProblem<double> pr = *mpp;
+        COMPI::FuncCnt<double> *obj = dynamic_cast<COMPI::FuncCnt<double>*> ((pr).mObjectives[0]);
+        cout << "inside331\n";
+        obj->reset();
+        cout << "inside333\n";
+        
+        
+        desk.search(x, v);
+         
+        std::cout << "In " << obj->mCounters.mFuncCalls << " function calls found v = " << v << "\n";
+        std::cout << " at " << snowgoose::VecUtils::vecPrint(n, x) << "\n";
+        cout << "inside5\n";
+
+        ASSERT_NEAR(expected, v, epsilon);
+    }
+    
+    DescFuncReader dfr;
 };
 
-
-	 
-TEST_F(GlobOptTest, TestGlobOptAckley1)
-{
+TEST_F(GlobOptTest, TestGlobOptAckley1) {
     int N = 3;
-	auto expr = Ackley1<double>(N);
+    auto expr = Ackley1<double>(N);
     auto exprInterval = Ackley1<Interval<double>>(N);
     testglobopt(K.Ackley1, expr, exprInterval, N);
 }
 
-TEST_F(GlobOptTest, TestGlobOptAckley2)
-{
-	int N = 4;
-	auto expr = Ackley2<double>(N);
+TEST_F(GlobOptTest, TestGlobOptAckley2) {
+    int N = 4;
+    auto expr = Ackley2<double>(N);
     auto exprInterval = Ackley2<Interval<double>>(N);
-	testglobopt(K.Ackley2, expr, exprInterval, N);
+    testglobopt(K.Ackley2, expr, exprInterval, N);
+}
+
+TEST_F(GlobOptTest, TestGlobOptAckley3)
+{
+	auto expr = Ackley3<double>();
+    auto exprInterval = Ackley3<Interval<double>>();
+	testglobopt(K.Ackley3, expr, exprInterval);
 }
 
 
+TEST_F(GlobOptTest, TestGlobOptAdjiman)
+{
+    auto expr = Adjiman<double>();
+    auto exprInterval = Adjiman<Interval<double>>();
+	testglobopt(K.Adjiman, expr, exprInterval);
+}
+
+TEST_F(GlobOptTest, TestGlobOptAlpine1)
+{  
+    int N = 3;
+    auto expr = Alpine1<double>(N);
+    auto exprInterval = Alpine1<Interval<double>>(N);
+    testglobopt(K.Alpine1, expr, exprInterval, N);
+}
 
 #endif /* UTESTGLOBOPT_HPP */
 
